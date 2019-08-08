5f29589168324ba1c5b719f9599d3c8cf031d63c
recursively <reindeer.zyh@gmail.com>
2019-08-08 22:43:43 +0800

    unittest updated

--- tests/Analyzer_test.py
+++ tests/Analyzer_test.py
@@ -10,15 +10,15 @@ class StockAnalyzerTestCase(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(__class__, self).__init__(*args, **kwargs)
         self.stock_code_cn = '600519'
+        self.Analyzer_cn = StockAnalyzerA(config.base_url_A, config.query_A, config.bonus_url_A, config.debt_url_A)
 
     def test_bonus_fetch_cn(self):
-        self.Analyzer = StockAnalyzerA(config.base_url_A, config.query_A, config.bonus_url_A, config.debt_url_A)
-        bonus_list = self.Analyzer.extract_bonus(self.stock_code_cn)
+        bonus_list = self.Analyzer_cn.extract_bonus(self.stock_code_cn)
         assert len(bonus_list) == 5
 
     def test_main_filter(self):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.Analyzer.iwc_filter())
+        loop.run_until_complete(self.Analyzer_cn.iwc_filter())
         random_item = self.stock_dict.popitem()
         assert type(int(random_item[0].split()[-1])) == int
 
