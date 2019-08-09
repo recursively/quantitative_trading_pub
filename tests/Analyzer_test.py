29151b0d9b4a35b8cb84d2d2624702f11019a15f
Eric_ZYH <reindeer.zyh@gmail.com>
2019-08-09 20:56:58 +0800

    unittest updated

--- tests/Analyzer_test.py
+++ tests/Analyzer_test.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock, patch
-from analyzer import StockAnalyzer, Quote, myfunc
+from analyzer import StockAnalyzer
 from stock_filter import StockAnalyzerA, StockAnalyzerHK, StockAnalyzerUS
 import config
 import asyncio
